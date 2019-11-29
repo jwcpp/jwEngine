de04fb31547e0af80d87b97b09d140d462708037
jwcpp <jw_cpp@163.com>
2019-11-29 16:28:30 +0800

    add csv parser

---
+++ common/CsvParser.cpp
@@ -0,0 +1,66 @@
+#include "CsvParser.h"
+#include <fstream>
+#include "XLog.h"
+
+
+CsvParser::CsvParser(const char * file, int startline, char sep) :
+	_sep(sep)
+{
+	std::string line;
+
+	std::ifstream ifile(file);
+	if (ifile.is_open())
+	{
+		int row = 0;
+		while (ifile.good())
+		{
+			getline(ifile, line);
+			if (line == "")
+				continue;
+
+			if (startline && --startline)
+				continue;
+
+			row++;
+			if (row == 1)
+			{
+				parseHeader(line);
+			}
+			else
+			{
+				parseContent(line);
+			}
+		}
+		ifile.close();
+	}
+	else
+	{
+		ERROR_LOG("Failed to open %s", file);
+	}
+}
+
+
+CsvParser::~CsvParser()
+{
+}
+
+void CsvParser::parseHeader(std::string & line)
+{
+	std::stringstream ss(line);
+	std::string item;
+
+	while (std::getline(ss, item, _sep))
+		_header.push_back(item);
+}
+
+void CsvParser::parseContent(std::string & line)
+{
+	std::stringstream ss(line);
+	std::string item;
+
+	CsvRow csvrow;
+	while (std::getline(ss, item, _sep))
+		csvrow.push(item);
+
+	_content.push_back(csvrow);
+}
---
+++ common/CsvParser.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+# include <sstream>
+#include <vector>
+
+class CsvRow
+{
+public:
+	unsigned int size(void) const{ return _values.size(); }
+	void push(const std::string & title){ _values.push_back(title); }
+
+	template<typename T>
+	const T getValue(int idx, T defvalue = T()) const
+	{
+		T res = defvalue;
+		std::stringstream ss;
+		ss << _values[idx];
+		ss >> res;
+		return res;
+	}
+private:
+	std::vector<std::string> _values;
+};
+
+class CsvParser
+{
+public:
+	CsvParser(const char * file, int startline = 1, char sep = '\t');
+	~CsvParser();
+
+	template<typename T>
+	const T getValue(int row, std::string name, T defvalue = T()) const
+	{
+		for (unsigned int i = 0; i < _header.size();++i)
+		{
+			if (name == _header[i])
+			{
+				const CsvRow & csvrow = _content[row - 1];
+				T ret = csvrow.getValue<T>(i, defvalue);
+				return ret;
+			}
+		}
+		
+		return T();
+	}
+
+protected:
+	void parseHeader(std::string & line);
+	void parseContent(std::string & line);
+private:
+	const char _sep;
+	std::vector<std::string> _header;
+	std::vector<CsvRow> _content;
+};
+
