3fcd9e7f8216da74d7148a29aae49365dfbaf37c
jwcpp <jw_cpp@163.com>
2021-04-04 14:06:37 +0800

    net.py

--- example/python/net.py
+++ example/python/net.py
@@ -2,6 +2,7 @@ import threading
 import queue
 import struct
 import socket
+import time
 
 HEAD_SIZE = 8
 class TcpClientBase(threading.Thread):
@@ -22,22 +23,32 @@ class TcpClientBase(threading.Thread):
 
     def connect(self):
         self.socket = socket.socket()
-        self.socket.connect((self.ip, self.port))
-        self.socket.setblocking(False)
+        try:
+            self.socket.connect((self.ip, self.port))
+        except ConnectionRefusedError:
+            self.socket.close()
+            self.socket = None
 
+        if self.socket:
+            self.socket.setblocking(False)
+    
     def __startRecv(self, size):
         try:
             recvdata = self.socket.recv(size)
-        except:
+        except BlockingIOError:
             recvdata = None
+        except:
+            return -1
 
         if recvdata:
             self.curread += recvdata
             self.readpos += len(recvdata)
+        return 0
 
     def __handleRead(self):
+        ret = 0
         if self.readpos < HEAD_SIZE:
-            self.__startRecv(HEAD_SIZE - self.readpos)
+            ret = self.__startRecv(HEAD_SIZE - self.readpos)
 
         if self.readpos >= HEAD_SIZE:
             bodylen = struct.unpack_from('i', self.curread, 0)[0]
@@ -46,7 +57,8 @@ class TcpClientBase(threading.Thread):
                 self.curread = bytearray()
                 self.readpos = 0
             else:
-                self.__startRecv(HEAD_SIZE + bodylen - self.readpos)
+                ret = self.__startRecv(HEAD_SIZE + bodylen - self.readpos)
+        return ret
 
 
     def __handleWrite(self):
@@ -55,15 +67,24 @@ class TcpClientBase(threading.Thread):
         if self.cursend is None:
             return
 
-        self.sendpos += self.socket.send(self.cursend[self.sendpos:])
+        sendsize = self.socket.send(self.cursend[self.sendpos:])
+        if sendsize > 0:
+            self.sendpos += sendsize
         if self.sendpos == len(self.cursend):
             self.sendpos = 0
             self.cursend = None
 
     def run(self):
         while True:
-            self.__handleWrite()
-            self.__handleRead()
+            if self.socket:
+                self.__handleWrite()
+                ret = self.__handleRead()
+                if ret < 0:
+                    self.socket = None
+            else:
+                time.sleep(1)
+                print('Reconnect...')
+                self.connect()
 
 
     def send(self, buff):
@@ -75,7 +96,8 @@ class TcpClientBase(threading.Thread):
         return self.reads.get_nowait()
 
     def close(self):
-        self.socket.close()
+        if self.socket:
+            self.socket.close()
 
 
 
@@ -89,10 +111,17 @@ class TcpClient(TcpClientBase):
 
     def start(self):
         TcpClientBase.connect(self)
+        # start thread
         TcpClientBase.start(self)
 
     def update(self):
+        if self.on_msg is None:
+            return
+
         buff = self.getBuff()
-        if buff and self.on_msg:
-            msgid = struct.unpack_from('i', buff, 4)[0]
-            self.on_msg(msgid, buff)
+        if buff is None:
+            return
+
+        msgid = struct.unpack_from('i', buff, 4)[0]
+        self.on_msg(msgid, buff)
+            
